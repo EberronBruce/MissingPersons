40f872c4790bf7fa6d08df56b4d52cfb316c0b86
Bruce Burgess <bruceburgess67@yahoo.com>
2016-05-13 21:12:34 +0700

    Connected the UICollectionView up

---
+++ MissingPersons/PersonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonCell.swift
+//  MissingPersons
+//
+//  Created by Bruce Burgess on 5/13/16.
+//  Copyright © 2016 Red Raven Computing Studios. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+}
